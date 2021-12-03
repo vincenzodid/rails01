465a7214f4e075006d6bafe59dc67de4dd544d4a
vincenzodid <v.didonna@donboscosandona.it>
2021-12-03 10:47:46 +0000

    commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    def index
+        # Abbiamo definito l'azione index del controller Home,
+        # in realta questa azione non fa niente,
+        # ma richiama la vista (view) corrispondente,
+        # che poi vieve inviata all'utente sotto forma di HTML
+    end
+end
+
+
